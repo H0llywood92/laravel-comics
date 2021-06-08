8d5045a1fe2c0e757b171d2e968c720a4e570dce
H0llywood92 <manuele.volponi@gmail.com>
2021-06-08 17:04:36 +0200

    header finito

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,17 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 
-    @include('header')
+    @include('partials.header')
 
     <main>
         @yield('main_content')
     </main>
 
-    @include('footer')
+    @include('partials.footer')
     
 </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,91 @@
+<header>
+
+    <div class="top-row-menu">
+        <div class="container">
+            <ul class="flex">
+                <li>
+                    dc power<span class="sm">sm</span> visa<span class="icon"><i class="far fa-registered"></i></span>
+                </li>
+
+                <li>
+                    addictional dc sites <span><i class="fas fa-caret-down"></i></span>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="header-content">
+
+        <div class="container flex">
+
+            <div class="logo">
+                <img src=" {{ asset('img/dc-logo.png') }} " alt="brand logo">
+            </div>
+    
+            <nav>
+
+                <ul class="menu flex">
+
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+
+                    <li>
+                        <a class="active" href="#">comics</a>
+                    </li>
+
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+
+                    <li>
+                        <a href="#">shop <span><i class="fas fa-caret-down"></i></span></a>
+                    </li>
+
+
+                </ul>
+    
+            </nav>
+
+            <div class="search-content">
+                <input type="text" class="search" placeholder="Search ">
+                <i class="fas fa-search"></i>
+            </div>
+            
+
+        </div>
+
+    </div>
+
+
+    <section class="jumbotron">
+
+        <img src=" {{ asset('img/jumbotron.jpg') }}" alt="">
+
+    </section>
+    
+</header>
